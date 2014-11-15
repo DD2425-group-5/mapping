2752e8c04bb53b5397ac0aea7c6b2bdc604a9bf3
Michal Staniaszek <m.staniaszek@gmail.com>
2014-11-15 14:56:11 +0100

    Added messages for the mapping to make serialisation easier
    Should be able to serialise the segments easily using built in ROS
    serialisation. Can store messages in a rosbag written from the node rather than
    a topic, and then read back in when necessary.
    
      # Please enter the commit
    message for your changes. Lines starting # with '#' will be ignored, and an
    empty message aborts the commit.  # On branch develop # Changes to be committed:
    ../mapping_msgs/CMakeLists.txt # new file: ../mapping_msgs/msg/MapSegment.msg #
    new file: ../mapping_msgs/msg/SegmentPoint.msg # new file:
    ../mapping_msgs/package.xml # modified: CMakeLists.txt # modified: package.xml #
    deleted: src/mapSegment.cpp # modified: src/segmentStorage.cpp #

--- segment_storage/src/segmentStorage.cpp
+++ segment_storage/src/segmentStorage.cpp
@@ -1,7 +1,10 @@
 #include <ros/ros.h>
+#include "hardware_msgs/IRDists.h"
+#include "mapping_msgs/MapSegment.h"
+#include "mapping_msgs/SegmentPoint.h"
 
 int main(int argc, char *argv[])
 {
-    
+
     return 0;
 }
