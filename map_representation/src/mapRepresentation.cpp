9952c4955b024771ecfad150f0c7b6735625ba52
Michal Staniaszek <m.staniaszek@gmail.com>
2014-11-23 18:15:22 +0100

    Added new message, representation now expects lines received to be grouped in segments

--- map_representation/src/mapRepresentation.cpp
+++ map_representation/src/mapRepresentation.cpp
@@ -6,55 +6,126 @@ namespace ocutil = occupancy_grid_utils;
 MapRepresentation::MapRepresentation(int argc, char *argv[]){
     ros::init(argc, argv, "map_representation");
     ros::NodeHandle handle;
+    
     std::string lineTopic;
     ROSUtil::getParam(handle, "/topic_list/mapping_topics/segment_stitching/published/stitched_line_topic",
                       lineTopic);
+    line_sub = handle.subscribe(lineTopic, 1, &MapRepresentation::lineCallback, this);
     
     ROSUtil::getParam(handle, "/map_representation/grid_resolution", grid.info.resolution);
-    line_sub = handle.subscribe(lineTopic, 1, &MapRepresentation::lineCallback, this);
-
+    
     runNode();
 }
 
 void MapRepresentation::runNode(){
-    
+    ros::Rate loopRate(10);
+    // wait to receive lines from the topic
+    while (!receivedLines && ros::ok()){
+        loopRate.sleep();
+        ros::spinOnce();
+    }
+
+    // received lines, so process them and populate the occupancy grid
+    populateGrid();
 }
 
-void MapRepresentation::lineCallback(const mapping_msgs::LineVector& msg){
-    lines = msg;
+void MapRepresentation::lineCallback(const mapping_msgs::SegmentLineVector& msg){
+    segLineVec = msg;
+    receivedLines = true; // start processing the lines to create the map
+}
+
+void MapRepresentation::populateGrid(){
+    std::vector<mapping_msgs::LineVector> segmentLines = segLineVec.segments;
+    MinMaxXY gridBounds = findSegmentBounds(segmentLines);
+    
+    // Set the width and height of the grid in cells based on the bounds of the
+    // lines provided, and the resolution of the grid.
+    float widthMetres = gridBounds.maxX - gridBounds.minX;
+    float heightMetres = gridBounds.maxY - gridBounds.minY;
+    grid.info.width = (int)(std::ceil(widthMetres/grid.info.resolution));
+    grid.info.height = (int)(std::ceil(heightMetres/grid.info.resolution));
+    // initialise the grid data, with each cell undefined.
+    grid.data = std::vector<signed char>(grid.info.width * grid.info.height, -1);
+    
+    // iterate over all the lines received 
+    std::vector<mapping_msgs::LineVector>::iterator it = segmentLines.begin();
+    for (; it != segmentLines.end(); it++) {
+        mapping_msgs::LineVector segment = *it;
+        // Find the bounds of a single segment
+        MinMaxXY segmentBounds = findSegmentBounds(segment);
+        // get the polygon representing the bound, will be a rectangle
+        geometry_msgs::Polygon bound = boundsToPolygon(segmentBounds);
+        // set the value of all cells in the polygon to empty
+        setCellsInBounds(bound, 0);
+        // for each individual line (wall) in the segment, set cells which fall under
+        // the line to occupied.
+        std::vector<mapping_msgs::Line> linesInSegment = segment.lines;
+        for (size_t i = 0; i < linesInSegment.size(); i++){
+            setCellsOnLine(linesInSegment[i], 100);
+        }
+    }
 }
 
 /**
- * Find the size of the bounding box of the set of lines given, in metres.
+ * Find min and max of the set of linevectors given, in metres. Basically the
+ * bounding box.
  */
-std::pair<float, float> MapRepresentation::findLineBoundSize(mapping_msgs::LineVector lineVec) {
-    std::vector<mapping_msgs::Line> lines=lineVec.lines;
-
+MinMaxXY MapRepresentation::findSegmentBounds(std::vector<mapping_msgs::LineVector> segmentLines) {
     float minX = std::numeric_limits<float>::max();
     float maxX = -std::numeric_limits<float>::max();
     float minY = std::numeric_limits<float>::max();
     float maxY = -std::numeric_limits<float>::max();
-    for(size_t lineIterator = 0; lineIterator<lines.size(); lineIterator++){
-        MinMaxXY minmax = lineMinMax(lines[lineIterator]);
-        if (minmax.minX < minX){
-            minX = minmax.minX;
-        }
-        if (minmax.maxX > maxX){
-            maxX = minmax.maxX;
-        }
-        if (minmax.minY < minY){
-            minY = minmax.minY;
-        }
-        if (minmax.maxY > maxY){
-            maxY = minmax.maxY;
-        }
+
+    for(size_t segmentInd = 0; segmentInd < segmentLines.size(); segmentInd++){
+        mapping_msgs::LineVector lines = segmentLines[segmentInd];
+        MinMaxXY minmax = findSegmentBounds(lines);
+        setMinMaxFromStruct(minmax, minX, maxX, minY, maxY);
     }
 
-    float L1normX = maxX - minX;
-    float L1normY = maxY - minY;
-
-    return std::pair<float, float> (L1normX, L1normY);
+    return MinMaxXY(minX, maxX, minY, maxY);
 }
+
+/**
+ * Find the min and max of a single vector of lines, returning the bounding box
+ * of those lines.
+ */
+MinMaxXY MapRepresentation::findSegmentBounds(mapping_msgs::LineVector segmentLine){
+    float minX = std::numeric_limits<float>::max();
+    float maxX = -std::numeric_limits<float>::max();
+    float minY = std::numeric_limits<float>::max();
+    float maxY = -std::numeric_limits<float>::max();
+    
+    std::vector<mapping_msgs::Line> lines = segmentLine.lines;
+
+    for (size_t lineInd = 0; lineInd < lines.size(); lineInd++){
+        MinMaxXY minmax = lineMinMax(lines[lineInd]);
+        setMinMaxFromStruct(minmax, minX, maxX, minY, maxY);
+    }
+        
+    return MinMaxXY(minX, maxX, minY, maxY);
+}
+
+/**
+ * Sets the values of minX, maxX, minY, maxY by comparing to the given MinMaxXY
+ * struct. if any of the values are less than or equal to their counterparts
+ * given in the parameters, their values are modified to correspond to values in
+ * the struct.
+ */
+void MapRepresentation::setMinMaxFromStruct(MinMaxXY comp, float& minX, float& maxX, float& minY, float& maxY){
+    if (comp.minX < minX){
+        minX = comp.minX;
+    }
+    if (comp.maxX > maxX){
+        maxX = comp.maxX;
+    }
+    if (comp.minY < minY){
+        minY = comp.minY;
+    }
+    if (comp.maxY > maxY){
+        maxY = comp.maxY;
+    }
+}
+
 
 /**
  * Find the minimum and maximum values of x and y on the specified line.
@@ -69,39 +140,27 @@ MinMaxXY MapRepresentation::lineMinMax(mapping_msgs::Line l){
 }
 
 /**
- * Gets the bounding box of the two lines provided. Takes into account the fact
+ * Gets the bounding box of the lines provided. Takes into account the fact
  * that the lines may be of different length and orientation.
  */
-geometry_msgs::Polygon MapRepresentation::boundsFromLines(mapping_msgs::Line l1, mapping_msgs::Line l2){
+geometry_msgs::Polygon MapRepresentation::boundsToPolygon(MinMaxXY bounds){
     geometry_msgs::Polygon poly;
     
-    // First find the bounding box of the lines - this is not just the end
-    // points of the lines. Need to actually extract the min and max values,
-    // because lines can be of different lengths.
-    MinMaxXY l1mm = lineMinMax(l1);
-    MinMaxXY l2mm = lineMinMax(l2);
-    
-    // min and max values when looking at both lines
-    float minX = l1mm.minX < l2mm.minX ? l1mm.minX : l2mm.minX;
-    float maxX = l1mm.maxX > l2mm.maxX ? l1mm.maxX : l2mm.maxX;
-    float minY = l1mm.minY < l2mm.minY ? l1mm.minY : l2mm.minY;
-    float maxY = l1mm.maxY > l2mm.maxY ? l1mm.maxY : l2mm.maxY;
-
     geometry_msgs::Point32 botLeft;
-    botLeft.x = minX;
-    botLeft.y = minY;
+    botLeft.x = bounds.minX;
+    botLeft.y = bounds.minY;
     botLeft.z = 0;
     geometry_msgs::Point32 topLeft;
-    botLeft.x = minX;
-    botLeft.y = maxY;
+    botLeft.x = bounds.minX;
+    botLeft.y = bounds.maxY;
     botLeft.z = 0;
     geometry_msgs::Point32 topRight;
-    botLeft.x = maxX;
-    botLeft.y = maxY;
+    botLeft.x = bounds.maxX;
+    botLeft.y = bounds.maxY;
     botLeft.z = 0;
     geometry_msgs::Point32 botRight;
-    botLeft.x = maxX;
-    botLeft.y = minY;
+    botLeft.x = bounds.maxX;
+    botLeft.y = bounds.minY;
     botLeft.z = 0;
     
     // push points onto the polygon in an order where they make a rectangle.
@@ -132,12 +191,12 @@ void MapRepresentation::setCellsInBounds(geometry_msgs::Polygon bounds, signed c
  * Projects a line onto the grid, setting cells which fall on the line to the
  * specified value.
  */
-void MapRepresentation::projectLineOntoGrid(mapping_msgs::Line lineToProject, signed char value){
-    ocutil::RayTraceIterRange iterators = ocutil::rayTrace( grid.info,
-                                                            lineToProject.start,
-                                                            lineToProject.end,
-                                                            false,
-                                                            false);
+void MapRepresentation::setCellsOnLine(mapping_msgs::Line lineToProject, signed char value){
+    ocutil::RayTraceIterRange iterators = ocutil::rayTrace(grid.info,
+                                                           lineToProject.start,
+                                                           lineToProject.end,
+                                                           false,
+                                                           false);
     ocutil::RayTraceIterator iterator = iterators.first;
     ocutil::RayTraceIterator iteratorEnd = iterators.second;
     
--- map_representation/src/mapRepresentation.hpp
+++ map_representation/src/mapRepresentation.hpp
@@ -1,8 +1,10 @@
 #include <ros/ros.h>
 #include <rosutil/rosutil.hpp>
 #include <utility>
+#include <cmath>
 #include <limits>
 #include <mapping_msgs/LineVector.h>
+#include <mapping_msgs/SegmentLineVector.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/Polygon.h>
 #include <occupancy_grid_utils/coordinate_conversions.h>
@@ -22,17 +24,24 @@ public:
 private:
     ros::Subscriber line_sub;
 
-    mapping_msgs::LineVector lines;
+    mapping_msgs::SegmentLineVector segLineVec;
     nav_msgs::OccupancyGrid grid;
+
+    bool receivedLines;
     
     void runNode();
-    void lineCallback(const mapping_msgs::LineVector& msg);
-    void projectLineOntoGrid(mapping_msgs::Line msg, signed char value);
+    void lineCallback(const mapping_msgs::SegmentLineVector& msg);
 
-    std::pair<float, float> findLineBoundSize(mapping_msgs::LineVector lines);
+    void populateGrid();
 
+
+
+    MinMaxXY findSegmentBounds(std::vector<mapping_msgs::LineVector> segmentLines);
+    MinMaxXY findSegmentBounds(mapping_msgs::LineVector segmentLine);
+    void setMinMaxFromStruct(MinMaxXY comp, float& minX, float& maxX, float& minY, float& maxY);
     void setCellsInBounds(geometry_msgs::Polygon bounds, signed char value);
-    geometry_msgs::Polygon boundsFromLines(mapping_msgs::Line l1, mapping_msgs::Line l2);
+    void setCellsOnLine(mapping_msgs::Line l, signed char value);
+    geometry_msgs::Polygon boundsToPolygon(MinMaxXY bounds);
     MinMaxXY lineMinMax(mapping_msgs::Line l);
 };
 
